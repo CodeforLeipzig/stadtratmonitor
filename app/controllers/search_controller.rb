c20a77dfa7feca5ac5b3e639beeef173ca7d1d4b
Andreas Haller <andreashaller@gmail.com>
2014-11-19 22:33:25 +0100

    Show 30 results by default

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @result = if params[:q].present?
       es = Elasticsearch::Client.new
-      es.search index: 'loris', q: params[:q]
+      es.search index: 'loris', q: params[:q], size: 30
     end
   end
 end
