553c1e653c9cf217e43e152ede852149a2594cad
Andreas Haller <andreashaller@gmail.com>
2015-04-27 23:25:14 +0200

    Show the number of all papers on homepage

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,12 @@
 class SearchController < ApplicationController
   def index
     @show_search_result = params[:q].present?
-    @papers = if @show_search_result
-      Paper.search(params[:q]).page(params[:page]).results
+    papers_found = if @show_search_result
+      Paper.search(params[:q])
     else
-      Paper.order(published_at: :desc).page(params[:page])
+      Paper.order(published_at: :desc)
     end
+    @papers_count = @show_search_result ? papers_found.results.total : papers_found.count
+    @papers = papers_found.page(params[:page])
   end
 end
