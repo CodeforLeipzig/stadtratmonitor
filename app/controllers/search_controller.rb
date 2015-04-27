f71c0837229a74a0f90ad3c2e5ca6782899a3242
Andreas Haller <andreashaller@gmail.com>
2015-04-27 22:35:11 +0200

    Show last 30 papers on homepage

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @result = if params[:q].present?
+    @show_search_result = params[:q].present?
+    @papers = if @show_search_result
       # TODO: Add pagination
       Paper.search(params[:q]).records
+    else
+      Paper.last(30)
     end
   end
 end
