4aa0b2424bd9b552f9bbc5d3ceb9a51b9919a5dd
Lars Henrik Mai <lars.mai@kontinui.de>
2015-06-22 20:58:25 +0200

    refactor facets_list helper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,9 +2,9 @@ module SearchHelper
   def facet_list(facets)
     return unless facets.present?
     content_tag(:ul) do
-      facets.each do |term, count|
+      facets.each do |facet|
         concat content_tag(:li,
-                           "#{term} (#{count})",
+                           "#{facet['key']} (#{facet['doc_count']})",
                            class: "facet"
                           )
       end
