631dca322cc8958e4843a0f50f746a8e10115fb4
Lars Henrik Mai <lars.mai@kontinui.de>
2015-09-27 13:51:13 +0200

    Refactor and expand Paper validations

---
+++ lib/parseable_date_validator.rb
@@ -0,0 +1,12 @@
+class ParseableDateValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attribute, value)
+    raw_value = record.read_attribute_before_type_cast(attribute)
+    return nil if raw_value.nil?
+    Date.parse(raw_value)
+    nil
+  rescue ArgumentError => e
+    record.errors[attribute] << (options[:message] || I18n.t("errors.messages.unparseable_date"))
+  end
+
+end
