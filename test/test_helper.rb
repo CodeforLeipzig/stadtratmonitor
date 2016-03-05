a247ccc70a712b97869309aa5d2feaf0e1e90b98
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2016-03-05 21:28:32 +0100

    use Coveralls.merged!('rails')

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.merged!('rails')
+
 require 'simplecov'
 SimpleCov.start 'rails'
 
@@ -5,9 +8,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
-
-require 'coveralls'
-Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
