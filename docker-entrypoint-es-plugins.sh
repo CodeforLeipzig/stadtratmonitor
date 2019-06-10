1612893b17ee770507ce1025ae72bef9aef90296
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2019-06-10 15:04:02 +0200

    Add missing css height for leaflet

--- docker-entrypoint-es-plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./bin/elasticsearch-plugin install http://xbib.org/repository/org/xbib/elasticsearch/plugin/elasticsearch-analysis-decompound/5.4.3.0/elasticsearch-analysis-decompound-5.4.3.0-plugin.zip
-
-exec /docker-entrypoint.sh elasticsearch
