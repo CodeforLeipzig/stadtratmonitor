377d7d1ba87c02b98bfad2d99d07fa69eb358c69
Lars Henrik Mai <lars.mai@kontinui.de>
2020-03-09 11:44:19 +0100

    Remove unused test/ directory

--- test/integration/routes_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class RoutesTest < ActionDispatch::IntegrationTest
-  test "route test" do
-    assert_generates "/import", { :controller => "import", :action => "new_papers_callback" }
-    assert_generates "/", :controller => "search", :action => "index"
-  end
-end
