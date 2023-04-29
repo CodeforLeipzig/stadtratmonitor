14688ad7e15218bd1eca34f0fa2366860c926d47
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-04-30 00:56:46 +0200

    truncate content with record

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -79,8 +79,8 @@ class Paper < ActiveRecord::Base
   class << self
     def import_from_json(json_string)
       old_count = count
-      content = truncate(record['content'], 99998)
       JSON.parse(json_string).each do |record|
+        content = truncate(record['content'], 99998)
         attributes = {
           body: record['body'],
           content: content,
